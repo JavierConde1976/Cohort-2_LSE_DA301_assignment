3218efd479d457e51d2a2bbcb4f74427cac08620
Javier Conde <102264964+JavierConde1976@users.noreply.github.com>
2022-09-11 00:08:08 +0100

    Add files via upload

--- LSE_DA301_assignment_R_Script_Javier_Conde.R
+++ LSE_DA301_assignment_R_Script_Javier_Conde.R
@@ -97,64 +97,10 @@ qplot(NA_Sales, data=turtle_sales2, colour=I('orange'),
 
 ###############################################################################
 
-# 3. Determine the impact on sales per product_id.
+# 3. Observations and insights
 
-## 3a) Use the group_by and aggregate functions.
-# Group data based on Product and determine the sum per Product.
-
-turtle_sales_product <- turtle_sales %>% group_by(Product) %>%
-  summarise(across(.cols = c('NA_Sales', 'EU_Sales', 'Global_Sales'), ~sum(.)))
-
-# View the data frame.
-
-as_tibble(turtle_sales_product)
-
-# Explore the data frame.
-
-summary(turtle_sales_product)
-
-View(turtle_sales_product)
-
-## 3b) Determine which plot is the best to compare game sales.
-# Create scatterplots.
-
-qplot(Global_Sales, EU_Sales, data=turtle_sales_product,
-      main='Scatterplot global sales vs EU sales grouped by product')
-
-qplot(Global_Sales, NA_Sales, data=turtle_sales_product,
-      main='Scatterplot global sales vs NA sales grouped by product')
-
-qplot(EU_Sales, NA_Sales, data=turtle_sales_product,
-      main='Scatterplot EU sales vs NA sales grouped by product')
-
-
-# Create histograms.
-
-qplot(Global_Sales, bins=25, data=turtle_sales_product, 
-      main='Histogram global sales grouped by product')
-qplot(EU_Sales, bins=25, data=turtle_sales_product, 
-      main='Histogram global sales grouped by product')
-qplot(NA_Sales, bins=25, data=turtle_sales_product, 
-      main='Histogram global sales grouped by product')
-
-# Create boxplots.
-
-qplot(Global_Sales, data=turtle_sales_product, colour=I('orange'), 
-      main='Boxplot global sales grouped by product', geom='boxplot')
-
-qplot(EU_Sales, data=turtle_sales_product, colour=I('orange'), 
-      main='Boxplot EU sales grouped by product', geom='boxplot')
-
-qplot(NA_Sales, data=turtle_sales_product, colour=I('orange'), 
-      main='Boxplot NA sales grouped by product', geom='boxplot')
-
-
-###############################################################################
-
-# 4. Observations and insights
-
-# relationship between partial sales and global sales
-# skewness
+# Relationship between partial sales and global sales.
+# Skewness:
 
 # Install the moments package and load the library.
 install.packages('moments') 
@@ -169,75 +115,101 @@ skewness(turtle_sales2$NA_Sales)
 # some outliers to watch (on recommendations)
 
 
-
 ###############################################################################
 ###############################################################################
 
 
-# Week 5: Cleaning and maniulating data using R -------------------------------
-
-## Utilising R, you will explore, prepare and explain the normality of the data
-## set based on plots, Skewness, Kurtosis, and a Shapiro-Wilk test. Note that
-## you will use this data set in future modules as well and it is, therefore, 
-## strongly encouraged to first clean the data as per provided guidelines and 
-## then save a copy of the clean data for future use.
-
-## Instructions
-# 1. Load and explore the data.
-##  - Continue to use the data frame that you prepared in the Week 4 assignment. 
-##  - View the data frame to sense-check the data set.
-##  - Determine the `min`, `max` and `mean` values of all the sales data.
-##  - Create a summary of the data frame.
-# 2. Determine the normality of the data set.
-##  - Create and explore Q-Q plots for all sales data.
-##  - Perform a Shapiro-Wilk test on all the sales data.
-##  - Determine the Skewness and Kurtosis of all the sales data.
-##  - Determine if there is any correlation between the sales data columns.
-# 3. Create plots to gain insights into the sales data.
-##  - Compare all the sales data (columns) for any correlation(s).
-##  - Add a trend line to the plots for ease of interpretation.
-# 4. Include your insights and observations.
-
-################################################################################
+# Week 5: Cleaning and manipulating data using R -------------------------------
 
 # 1. Load and explore the data
 
 # View data frame created in Week 4.
 
-turtle_sales_product
+turtle_sales2
 
-# View head, structore, descriptive statistics.
+# View head qnd structure
 
-head(turtle_sales_product)
-str(turtle_sales_product)
-summary(turtle_sales_product)
+head(turtle_sales2)
+str(turtle_sales2)
 
 
 # Check output: Determine the min, max, and mean values.
 .
-min(turtle_sales_product$Global_Sales)
-min(turtle_sales_product$EU_Sales)
-min(turtle_sales_product$NA_Sales)
+min(turtle_sales2$Global_Sales)
+min(turtle_sales2$EU_Sales)
+min(turtle_sales2$NA_Sales)
 
-max(turtle_sales_product$Global_Sales)
-max(turtle_sales_product$EU_Sales)
-max(turtle_sales_product$NA_Sales)
+max(turtle_sales2$Global_Sales)
+max(turtle_sales2$EU_Sales)
+max(turtle_sales2$NA_Sales)
 
-mean(turtle_sales_product$Global_Sales)
-mean(turtle_sales_product$EU_Sales)
-mean(turtle_sales_product$NA_Sales)
+mean(turtle_sales2$Global_Sales)
+mean(turtle_sales2$EU_Sales)
+mean(turtle_sales2$NA_Sales)
 
 
 # View the descriptive statistics.
 
-summary(turtle_sales_product)
+summary(turtle_sales2)
 
 
 ###############################################################################
 
-# 2. Determine the normality of the data set.
+# 2. Determine the impact on sales per product_id.
 
-## 2a) Create Q-Q Plots
+# 2a) Use the group_by and aggregate functions.
+# Group data based on Product and determine the sum per Product.
+
+turtle_sales_product <- turtle_sales %>% group_by(Product) %>%
+  summarise(across(.cols = c('NA_Sales', 'EU_Sales', 'Global_Sales'), ~sum(.)))
+
+# View the data frame.
+
+as_tibble(turtle_sales_product)
+
+# Summary and View of the new data frame.
+
+summary(turtle_sales_product)
+
+View(turtle_sales_product)
+
+## 2b) Create scatterplots, histograms and boxplots to gain insights into the sales data.
+# Scatterplots.
+
+qplot(Global_Sales, EU_Sales, data=turtle_sales_product,
+      main='Scatterplot global sales vs EU sales grouped by product')
+
+qplot(Global_Sales, NA_Sales, data=turtle_sales_product,
+      main='Scatterplot global sales vs NA sales grouped by product')
+
+qplot(EU_Sales, NA_Sales, data=turtle_sales_product,
+      main='Scatterplot EU sales vs NA sales grouped by product')
+
+
+# Histograms.
+
+qplot(Global_Sales, bins=25, data=turtle_sales_product, 
+      main='Histogram global sales grouped by product')
+qplot(EU_Sales, bins=25, data=turtle_sales_product, 
+      main='Histogram global sales grouped by product')
+qplot(NA_Sales, bins=25, data=turtle_sales_product, 
+      main='Histogram global sales grouped by product')
+
+# Boxplots.
+
+qplot(Global_Sales, data=turtle_sales_product, colour=I('orange'), 
+      main='Boxplot global sales grouped by product', geom='boxplot')
+
+qplot(EU_Sales, data=turtle_sales_product, colour=I('orange'), 
+      main='Boxplot EU sales grouped by product', geom='boxplot')
+
+qplot(NA_Sales, data=turtle_sales_product, colour=I('orange'), 
+      main='Boxplot NA sales grouped by product', geom='boxplot')
+
+
+# 3. Determine the normality of the data set.
+
+## 3a) Create Q-Q Plots
 # Create Q-Q Plots.
 
 qqnorm(turtle_sales_product$Global_Sales)
@@ -253,7 +225,7 @@ qqnorm(turtle_sales_product$NA_Sales)
 qqline(turtle_sales_product$NA_Sales, col='blue')
 
 
-## 2b) Perform Shapiro-Wilk test
+## 3b) Perform Shapiro-Wilk test
 # Install and import Moments.
 
 install.packages('moments') 
@@ -268,7 +240,7 @@ shapiro.test((turtle_sales_product$NA_Sales))
 # Our p-values are way below 0.05 (Global 2.2e-16, EU 2.987e-16, NA 2.2e-16),
 # so we can conclude that the sample data is not normally distributed.
 
-## 2c) Determine Skewness and Kurtosis
+## 3c) Determine Skewness and Kurtosis
 # Skewness and Kurtosis.
 
 skewness(turtle_sales_product$Global_Sales)
@@ -280,8 +252,12 @@ kurtosis(turtle_sales_product2$EU_Sales)
 kurtosis(turtle_sales_product2$NA_Sales)
 
 
-## 2d) Determine correlation
-# Determine correlation.
+## 3d) Determine correlation between the sales data columns
+
+round(cor(turtle_sales_product), digits=2)
+
+# Strong correlation between Global_sales and NA_sales (0.92), Global_sales and
+# EU_sales (0.85). Less good correlation between EU_sales and NA_sales.
 
 
 ###############################################################################
@@ -289,51 +265,56 @@ kurtosis(turtle_sales_product2$NA_Sales)
 # 3. Plot the data
 # Create plots to gain insights into data.
 
+# Scatterplot global sales vs NA sales
+
+ggplot(data=turtle_sales_product,mapping=aes(x=Global_Sales, y=NA_Sales)) +
+  geom_point(color='black',
+             alpha=0.75,
+             size=2.5) +
+  geom_smooth(method='lm', color='orange') +
+  scale_x_continuous("Global sales") +
+  scale_y_continuous("North America sales") +
+  labs(title="Turtle Games global sales vs North America sales (Million GBP)")
+
+
+# Scatterplot global sales vs Europe sales
+
+ggplot(data=turtle_sales_product,mapping=aes(x=Global_Sales, y=EU_Sales)) +
+  geom_point(color='black',
+             alpha=0.75,
+             size=2.5) +
+  geom_smooth(method='lm', color='orange') +
+  scale_x_continuous("Global sales") +
+  scale_y_continuous("Europe sales") +
+  labs(title="Turtle Games global sales vs North Europe sales(Million GBP)")
+
+# Scatterplot global sales vs Europe sales
+
+ggplot(data=turtle_sales_product,mapping=aes(x=EU_Sales, y=NA_Sales)) +
+  geom_point(color='black',
+             alpha=0.75,
+             size=2.5) +
+  geom_smooth(method='lm', color='orange') +
+  scale_x_continuous("Global sales") +
+  scale_y_continuous("Europe sales") +
+  labs(title="Turtle Games Europe sales vs North America sales(Million GBP)")
+
+
 
 ###############################################################################
 
 # 4. Observations and insights
-# Your observations and insights here...
+
+# Scatterplots very useful to understand sales variables relationships
+# Strong correlation between Global_sales and NA_sales (0.92), Global_sales and
+# EU_sales (0.85). Less good correlation between EU_sales and NA_sales.
+.
 
 
 ###############################################################################
 ###############################################################################
 
-# Week 6 assignment: Making recommendations to the business using R
-
-## The sales department wants to better understand if there is any relationship
-## between North America, Europe, and global sales. Therefore, you need to
-## investigate any possible relationship(s) in the sales data by creating a 
-## simple and multiple linear regression model. Based on the models and your
-## previous analysis (Weeks 1-5), you will then provide recommendations to 
-## Turtle Games based on:
-##   - Do you have confidence in the models based on goodness of fit and
-##        accuracy of predictions?
-##   - What would your suggestions and recommendations be to the business?
-##   - If needed, how would you improve the model(s)?
-##   - Explain your answers.
-
-# Instructions
-# 1. Load and explore the data.
-##  - Continue to use the data frame that you prepared in the Week 5 assignment. 
-# 2. Create a simple linear regression model.
-##  - Determine the correlation between the sales columns.
-##  - View the output.
-##  - Create plots to view the linear regression.
-# 3. Create a multiple linear regression model
-##  - Select only the numeric columns.
-##  - Determine the correlation between the sales columns.
-##  - View the output.
-# 4. Predict global sales based on provided values. Compare your prediction to
-#      the observed value(s).
-##  - NA_Sales_sum of 34.02 and EU_Sales_sum of 23.80.
-##  - NA_Sales_sum of 3.93 and EU_Sales_sum of 1.56.
-##  - NA_Sales_sum of 2.73 and EU_Sales_sum of 0.65.
-##  - NA_Sales_sum of 2.26 and EU_Sales_sum of 0.97.
-##  - NA_Sales_sum of 22.08 and EU_Sales_sum of 0.52.
-# 5. Include your insights and observations.
-
-###############################################################################
+# Week 6: Making recommendations to the business using R------------------------
 
 # 1. Load and explor the data
 # View data frame created in Week 5.
